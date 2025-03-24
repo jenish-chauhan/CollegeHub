ed0ef709d3ed488fd2cd2079d3888afef70e986d
Jay Varma <jayvarma1415@gmail.com>
2025-03-24 15:25:32 +0530

    j

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ require express from 'express';
 const app = express();
 require path from 'path';
 app.use(express.static(path.join(__dirname, 'public')));
+require mongoose from 'mongoose';
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
